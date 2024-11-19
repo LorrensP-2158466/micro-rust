3c76f6b36c7a87e5434305718ff7145657bd14e2
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2024-11-19 18:53:43 +0100

    the big refactor

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../lexer/lexer.yy.cpp"
+  "../parser/parser.tab.cpp"
+  "../parser/parser.tab.hpp"
+  "CMakeFiles/main.dir/lexer/lexer.yy.cpp.o"
+  "CMakeFiles/main.dir/lexer/lexer.yy.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/parser/parser.tab.cpp.o"
+  "CMakeFiles/main.dir/parser/parser.tab.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
